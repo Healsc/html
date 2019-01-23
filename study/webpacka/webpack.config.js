50f2095bff2fc4972a7061b5259d517fa2e2b592
sunchuang <873338654@qq.com>
2019-01-23 22:28:30 +0800

    study

--- study/webpacka/webpack.config.js
+++ study/webpacka/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const webpack = require("webpack");
 module.exports = {
     mode:'development',
     entry:{
@@ -8,4 +9,36 @@ module.exports = {
         path:path.resolve(__dirname,'dist'),
         filename:'[name].js'
     }
+
+
+  /*   module:{
+
+    }, */
+
+
+    plugins:[
+        new webpack.HotModuleReplacementPlugin(),
+        new htmlPlugin({
+          minify:{
+            removeAttributeQuotes:true,
+            collapseWhitespace:false
+          },
+          hash:true,
+          template:"./src/index.html"
+        }),
+        new ExtractTextPlugin("./css/main.css"),
+      ],
+
+    deServer:{
+        //设置基本目录结构
+        contentBase:path.resolve(__dirname,'dist'),
+        //服务器的IP地址
+        host:'localhost',
+        //服务端压缩是否开启
+        compress:true,
+        //服务端口号
+        port:8081,
+        hot:true
+    }
+    
 }

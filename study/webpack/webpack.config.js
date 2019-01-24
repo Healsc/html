26ffb3b419874ee584f7fe40a7fc1b386df11e41
sunchuang <873338654@qq.com>
2019-01-24 15:45:12 +0800

    study

--- study/webpack/webpack.config.js
+++ study/webpack/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const webpack = require('webpack');
 module.exports = {
     mode:'development',
     entry:{
@@ -7,7 +8,17 @@ module.exports = {
     output:{
         path:path.resolve(__dirname,'dist'),
         filename:'[name].js'
-    }
-   
-  
+    },
+    plugins:[
+        new webpack.HotModuleReplacementPlugin()
+    ]
+
+    devServer:{
+        contentBase:path.resolve(__dirname,'dist'),
+        host:'127.0.0.1',
+        compress:true,
+        port:'8081',
+        host:true,
+        open:true
+    }  
 }

3655bd7f041710e36cadca1941c2e462e949e586
sunchuang <873338654@qq.com>
2019-02-13 14:54:09 +0800

    vuecli

--- study/vuecli/testvuex/src/views/About.vue
+++ study/vuecli/testvuex/src/views/About.vue
@@ -1,16 +1,39 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
-    <h2>{{$store.state.count}}</h2>
-    <button @click="add">add</button>
+   
+    <h2>{{count}}</h2>
+    <h2>{{count1}}</h2>
+    <button @click="add(10)">add</button>
+    <button @click="addAction">add</button>
   </div>
 </template>
 <script>
+import {mapState,mapMutations,mapGetters,mapActions} from 'vuex';
 export default {
-  methods:{
+  /* methods:{
     add(){
       this.$store.commit('add');
     }
+  }, */
+  methods:{
+    ...mapMutations(['add']),
+    ...mapActions(['addAction'])
+  },
+    computed: {
+    ...mapState(['count']),
+    ...mapGetters(['count1'])
   }
+
+ /*  computed:mapState(["count"]) */
+/*   computed:mapState({
+    count:state=>state.count
+  }) */
+
+/*   computed:{
+    count(){
+      return this.$store.state.count;
+    }
+  } */
 }
 </script>

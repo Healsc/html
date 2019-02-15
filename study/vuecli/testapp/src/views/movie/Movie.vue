cf9dbb9b81d3ec6ff356cf55662c38bec29705d2
sunchuang <873338654@qq.com>
2019-02-15 23:35:12 +0800

    vue

--- study/vuecli/testapp/src/views/movie/Movie.vue
+++ study/vuecli/testapp/src/views/movie/Movie.vue
@@ -1,6 +1,11 @@
 <template>
   <div>
-    
+    <ul>
+        <li class="item-list"> 
+            <div class="list-left">1</div>
+            <div class="list-right"></div>
+        </li>
+    </ul>
   </div>
 </template>
 <script>
@@ -10,7 +15,21 @@ export default {
       title:"电影",
       className:"movie"
     }
-    this.$emit("changeNav",obj)
+    this.$emit("changeNav",obj);
+
+  },
+  methods:{
+      getData(){
+          let proxy = 'https://bird.ioliu.cn/v2?url='
+          let url = 'https://m.douban.com/rexxar/api/v2/subject_collection/movie_showing/items?start=0&count=10'
+          axios.get(proxy+url)
+          .then(()=>{
+         
+          })
+          .catch(()=>{
+             
+          })
+      }
   }
 }
 </script>

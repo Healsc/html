14068edf8d0eae99055286803077e818f9ef3081
sunchuang <873338654@qq.com>
2019-02-16 17:01:40 +0800

    vuecli

--- study/vuecli/testapp/src/views/chat/Chat.vue
+++ study/vuecli/testapp/src/views/chat/Chat.vue
@@ -1,8 +1,36 @@
 <template>
   <div>
-    
+    <div class="send-btn">
+      <input type="text">
+      <button @click="send">发送</button>
+    </div>
+    <div>
+      <ul>
+        <li>
+          <div class="right">
+            <span>right</span>
+          </div>
+          <div class="left">
+            <span>left</span>
+            </div>
+        </li>
+      </ul>
+    </div>
+
   </div>
 </template>
+<style scoped>
+  .send-btn{
+    height: 1rem;
+    width: 100%;
+    background-color: rgba(63, 81, 181,.7);
+    position: fixed;
+    bottom:1rem;
+    text-align: center;
+    line-height: 1rem;
+  }
+</style>
+
 <script>
 export default {
   created(){
@@ -11,6 +39,25 @@ export default {
       className:"chat"
     }
     this.$emit("changeNav",obj)
-  }
+  },
+  methods: {
+    getTimeStamp(){
+      var timestamp = Date.parse(new Date());
+      timestamp = timestamp.toString().substr(0,10);
+      return timestamp;
+    },
+    send(){
+      let params = {
+        app_id:"2111882338",
+        time_stamp:this.getTimeStamp(),
+        nonce_str:"",
+        sign:"",
+        session:"",
+        question:"",
+      }
+      console.log(params.time_stamp)
+      let appkey = "luAmnq5y8Q4f29Qj";
+    }
+  },
 }
 </script>

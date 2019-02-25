c305d79871cb90c33ab8af02bd46aa83676b0cd0
sunchuang <873338654@qq.com>
2019-02-25 21:02:09 +0800

    react

--- react/testa/src/App.js
+++ react/testa/src/App.js
@@ -9,8 +9,10 @@ import Class6 from './components/Class6'
 import Class7 from './components/Class7'
 import { 
   BrowserRouter as Router, 
-  Route, Link ,
-  Redirect
+  Route, 
+  Link ,
+  Redirect,
+  Switch
 } from "react-router-dom";
 import {Provider} from 'react-redux'
 import store from './srore'
@@ -23,6 +25,7 @@ const Class3 = ({match})=>{
     </div>
   )
 }
+const Error404 = ()=><h1>Error404</h1>
 class App extends Component {
   render() {
 
@@ -40,14 +43,17 @@ class App extends Component {
               <Link to="/class5">class5</Link>|
               <Link to="/class6">class6</Link>|
               <Link to="/class7">class7</Link>
-              <Route exact path={'/'} component={Class1}/>
-              <Route path={'/class2/:id'} component={Class2}/>
-              <Route path={'/class3/:id'} component={Class3}/>
-              <Route path={'/render'} render={()=><Redirect to="/"/>}/>
-              <Route path={'/class4'} component={Class4}/>
-              <Route path='/class5' component={Class5}/>
-              <Route path='/class6' component={Class6}/>
-              <Route path='/class7' component={Class7}/>
+              <Switch>
+                <Route exact path={'/'} component={Class1}/>
+                <Route path={'/class2/:id'} component={Class2}/>
+                <Route path={'/class3/:id'} component={Class3}/>
+                <Route path={'/render'} render={()=><Redirect to="/"/>}/>
+                <Route path={'/class4'} component={Class4}/>
+                <Route path='/class5' component={Class5}/>
+                <Route path='/class6' component={Class6}/>
+                <Route path='/class7' component={Class7}/>
+                <Route component={Error404}/>
+              </Switch>
           </div> 
         </Router>
         </Provider>
--- react/testa/src/components/Class7.js
+++ react/testa/src/components/Class7.js
@@ -9,24 +9,23 @@ import {
 const One =()=>{
     return(
         <div>
-            <h1>One</h1>
+            <h3>One</h3>
         </div>
     )
 }
 const Class7 = () => (
   <Router>
     <div>
-      <ul>
-        <li>
-          <Link to="/">Form</Link>
-        </li>
-        <li>
-          <Link to="/one">One</Link>
-        </li>
-        <li>
-          <Link to="/two">Two</Link>
-        </li>
-      </ul>
+        <div>
+            <Link to="/">Form</Link>
+        </div>
+        <div>
+            <Link to="/one">One</Link>
+        </div>
+        <div>
+            <Link to="/two">Two</Link>
+        </div> 
+        <hr/>  
       <Route path="/" exact component={Form} />
       <Route path="/one" component={One} />
       <Route path="/two" render={() => <h3>Two</h3>} />
@@ -34,21 +33,19 @@ const Class7 = () => (
   </Router>
 );
 class Form extends React.Component {
-  state = {
-    isBlocking: false
-  };
+    constructor(props){
+        super(props);
+        this.state = {
+        isBlocking: false
+    }
+  }
   render() {
     const { isBlocking } = this.state;
     return (
         <div>
-            <Prompt
-                when={isBlocking}
-                message={location =>
-                    `Are you sure you want to go to ${location.pathname}`
-                }
-            />
             <input
-                placeholder="type something to block transitions"
+                size="20"
+                placeholder="hello world"
                 onChange={(event) => {
                 this.setState({
                     isBlocking: event.target.value.length > 0
@@ -56,6 +53,12 @@ class Form extends React.Component {
                 }}
             />
             <button>Submit</button>
+            <Prompt
+                when={isBlocking}
+                message={location =>
+                    `Are you sure you want to go to ${location.pathname}`
+                }
+            />
         </div>
     );
   }

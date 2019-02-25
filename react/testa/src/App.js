124cfabdba9b675dd676cf6a50127b4945a9dc9d
sunchuang <873338654@qq.com>
2019-02-25 11:31:00 +0800

    react

--- react/testa/src/App.js
+++ react/testa/src/App.js
@@ -3,7 +3,12 @@ import React, { Component } from 'react';
 import './App.css';
 import Class1 from './components/Class1'
 import Class2 from './components/Class2'
-import { BrowserRouter as Router, Route, Link ,Redirect} from "react-router-dom";
+import Class4 from './components/Class4'
+import { 
+  BrowserRouter as Router, 
+  Route, Link ,
+  Redirect
+} from "react-router-dom";
 
 const Test = ()=><h1>hello world</h1>
 const Class3 = ({match})=>{
@@ -25,11 +30,13 @@ class App extends Component {
               <Link to="/">class1</Link>|
               <Link to="/class2/sunc">class2</Link>|
               <Link to={'/class3/100'}>class3</Link>|
-              <Link to="/render">render</Link>
+              <Link to="/render">render</Link>|
+              <Link to='/class4'>class4</Link>
               <Route exact path={'/'} component={Class1}/>
               <Route path={'/class2/:id'} component={Class2}/>
               <Route path={'/class3/:id'} component={Class3}/>
               <Route path={'/render'} render={()=><Redirect to="/"/>}/>
+              <Route path={'/class4'} component={Class4}/>
           </div> 
         </Router>
       </div>
---
+++ react/testa/src/components/Class4.js
@@ -0,0 +1,76 @@
+import React, { Component } from 'react'
+import { 
+    BrowserRouter as Router, 
+    Route,
+    Link ,
+    withRouter,
+    Prompt
+  } from "react-router-dom";
+const Page1 = ()=>{
+      return(
+          <div>
+              <h1>
+                  这是Page1
+              </h1>
+          </div>
+      )
+  }
+const Page2 = ()=>{
+    return(
+        <div>
+            <h1>
+                这是Page2
+            </h1>
+        </div>
+    )
+}
+const MenuLink = ({to,lable})=>{
+    return(
+        <Route 
+            path={to}
+            children={
+                ({match})=>{
+                    return(
+                        <div>
+                            <Link to={to}>{lable}</Link>
+                        </div>
+                    )
+                }
+            }
+        />
+    )
+}
+const RouterChange = withRouter(({history})=>{
+    return(
+      <div>
+        <button onClick={()=>{history.push("/page2")}}>编程导航</button>
+      </div>
+    )
+})
+
+export default class Class4 extends Component {
+    constructor(props){
+        super(props);
+        this.state={
+            prompt:true
+        }
+    }
+  render() {
+    return (
+      <div>
+        <Router>
+            <div>
+                <Link to="/page1">子链接1</Link>|
+                <Link to="/page2">子链接2</Link>
+                <MenuLink to="/page3" lable="自定义链接1"/>
+                <MenuLink to="page4" lable="自定义链接2"/>
+                <RouterChange/>
+                <Route path='/page1' component={Page1}/>
+                <Route path='/page2' component={Page2}/>
+                <Prompt when={this.state.prompt} message="确定离开？？？"/>
+            </div>
+        </Router>
+      </div>
+    )
+  }
+}

5cf85d2831521374a3adf47679ce94d27395cae3
healsc <873338654@qq.com>
2019-03-16 15:55:03 +0800

    a

---
+++ node/Class/testfile.js
@@ -0,0 +1,13 @@
+var fs = require('fs');
+fs.readFile('./file/test.text','utf-8',function(err,data){
+    if(err){
+        console.log("ERROR");
+    }else{
+        console.log(data);
+    }
+})
+console.log("end");
+
+//输出
+//end
+//hello world

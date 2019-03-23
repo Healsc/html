4cce496bb93744afff3c1352d25ba7feada21996
healsc <873338654@qq.com>
2019-03-23 21:18:16 +0800

    l

---
+++ node/Class/2_server.js
@@ -0,0 +1,15 @@
+var http = require('http')
+var url = require('url')
+http.createServer(function(req,res){
+    let path = url.parse(req.url).pathname;
+    if(path == '/'){
+        res.end('home')
+    }else if(path == '/about'){
+        res.end('about')
+    }else if(path == '/mine'){
+        res.end('mine')
+    }else{
+        res.end('ERROR404')
+    }
+}).listen('3000')
+console.log('running')

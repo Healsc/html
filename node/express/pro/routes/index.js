602d2bb9d0fdda2c12486a04b33516c691f20694
healsc <873338654@qq.com>
2019-03-26 12:20:15 +0800

    l

--- node/express/pro/routes/index.js
+++ node/express/pro/routes/index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-
+var User = require('../controllers/user.js');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
@@ -8,5 +8,24 @@ router.get('/', function(req, res, next) {
 router.get('/login', function(req, res, next) {
   res.render('login');
 });
+
+router.get('/check', function(req, res, next) {
+  res.render('check',{
+    'data':[{
+      'id':0,
+      'name':'zhangsan',
+      'age':10
+    },{
+      'id':1,
+      'name':'lisi',
+      'age':11
+    },{
+      'id':2,
+      'name':'zhaowu',
+      'age':13
+    }]
+  });
+});
+//router.get('/login', User.reg);
 
 module.exports = router;

4fb943b0fc011f2b66b4cdabeee7a45bee5680ff
Healsc <873338654@qq.com>
2019-08-19 08:40:36 +0800

    vuecli

--- restudy/vuecli/hello-vue/src/App.vue
+++ restudy/vuecli/hello-vue/src/App.vue
@@ -9,7 +9,9 @@
       <router-link to="/test/test2/ls/20">test2</router-link> |
        <router-link to="/home">返回首页</router-link> 
     </div>
-    <router-view/>
+    <transition name="fade">
+      <router-view/>
+    </transition>
   </div>
 </template>
 
@@ -21,5 +23,24 @@
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.fade-enter{
+  opacity: 0;
+}
+.fade-enter-active{
+  transition:all 0.5s ease 1s; 
+}
+
+.fade-enter-to{
+  opacity: 1;
+} 
+.fade-leave{
+  opacity: 1;
+}
+.fade-leave-active {
+  transition:all 1s ease; 
+}
+.fade-leave-to {
+  opacity: 0;
 }
 </style>

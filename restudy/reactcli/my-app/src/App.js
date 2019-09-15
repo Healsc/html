620f412341f1097d1828916e8d6f578026c97552
Healsc <873338654@qq.com>
2019-09-15 22:45:39 +0800

    0

--- restudy/reactcli/my-app/src/App.js
+++ restudy/reactcli/my-app/src/App.js
@@ -1,28 +1,29 @@
 import React from 'react';
 
 import './App.css';
-import Class1 from './components/Class1'
-import Class2 from './components/Class2'
+import Class1 from './components/Class1';
+import Class2 from './components/Class2';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+} from 'react-router-dom';
 function App() {
   return (
     
     <div className="App">
-      <Class1/>
-      <Class2/>
-     {/*  <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
+      <Router>
+        <div>
+          <Link to="/">class-one</Link> | 
+          <Link to="/class2/100">class-two</Link> |  
+          <Link to="/rendertest">class-three</Link>
+          <Route path={'/'} component={Class1}/>
+          <Route path={'/class2/:id'} component={Class2}/>
+          <Route path={'/rendertest'} render={()=><div><h1>hi render</h1></div>}></Route>
+        </div>
+      </Router>
+     {/*  <Class1/>
+      <Class2/> */}
     </div>
   );
 }
--- restudy/reactcli/my-app/src/components/Class2.js
+++ restudy/reactcli/my-app/src/components/Class2.js
@@ -2,8 +2,10 @@ import React, { Component } from 'react'
 
 export default class componentName extends Component {
   render() {
+    //console.log(this.props.match)
     return (
       <div>
+          
           <h1>hello world</h1>
       </div>
     )

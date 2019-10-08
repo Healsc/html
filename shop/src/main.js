f173bc2cb0fb66c07707b3caea5121e4707ad87f
Healsc <873338654@qq.com>
2019-10-08 20:50:48 +0800

    shop

--- shop/src/main.js
+++ shop/src/main.js
@@ -4,6 +4,19 @@ import router from './router'
 import store from './store'
 import '@/assets/css/reset.css'
 import '@/assets/js/rem.js'
+
+
+/* import Vant from 'vant';
+import 'vant/lib/index.css';
+
+Vue.use(Vant); */
+import { Button } from 'vant';
+Vue.use(Button);
+import { NavBar } from 'vant';
+Vue.use(NavBar);
+import { Icon } from 'vant';
+Vue.use(Icon);
+
 Vue.config.productionTip = false
 
 new Vue({
--- shop/src/views/Home.vue
+++ shop/src/views/Home.vue
@@ -1,5 +1,14 @@
 <template>
-  <div>hme</div>
+  <div>
+   <van-nav-bar
+      title="首页"
+     >
+    <van-icon name="search" slot="left" />  
+    <van-icon name="cart" slot="right"></van-icon>
+  </van-nav-bar>
+    
+  </div>
+  
 </template>
 
 <script>

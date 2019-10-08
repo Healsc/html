eae2e247ed819bfc9ce0e1f7b1f9acbce7596a7e
Healsc <873338654@qq.com>
2019-10-08 10:46:13 +0800

    t

--- shop/src/App.vue
+++ shop/src/App.vue
@@ -1,22 +1,18 @@
 <template>
   <div id="app">
-    <div class="box">hello vue</div>
+    <div>
+      <ul class="footbar">
+      <li>首页</li>
+      <li>分类</li>
+      <li>购物车</li>
+      <li>我的</li>
+    </ul>
+   
+    </div>
     
   </div>
 </template>
 
 <style lang="scss">
-  .box{
-    width: 100%;
-    height: 200px;
-    background-color:brown;
-    text-align: center;
-    line-height: 200px;
-  }
-  @media(max-width:600px){
-    .box{
-      width: 50%;
-      background-color:cadetblue;
-    }
-  }
+ 
 </style>

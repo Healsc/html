c4b1133cbd98aa464eac43fd48f56abf6768b1ff
Healsc <873338654@qq.com>
2019-10-19 19:40:33 +0800

    shop login regist

--- shop/src/views/Profile.vue
+++ shop/src/views/Profile.vue
@@ -5,10 +5,21 @@
       <van-tab title="登录">
         <van-cell-group>
           <van-field label="用户名" required clearable placeholder="请输入用户名" v-model="loginUserName"></van-field>
-          <van-field label="密码" required clearable placeholder="请输入密码" v-model="loginPassword"></van-field>
+          <van-field label="密码" type="password" required clearable placeholder="请输入密码" v-model="loginPassword"></van-field>
         </van-cell-group>
+        <div>
+          <van-button type="primary" size="large">登录</van-button>
+        </div>
       </van-tab>
-      <van-tab title="注册"></van-tab>
+      <van-tab title="注册">
+        <van-cell-group>
+          <van-field label="用户名" required clearable placeholder="请输入用户名" v-model="registUserName"></van-field>
+          <van-field label="密码" type="password" required clearable placeholder="请输入密码" v-model="registPassword"></van-field>
+        </van-cell-group>
+        <div>
+          <van-button type="primary" size="large">注册</van-button>
+        </div>
+      </van-tab>
     </van-tabs>
   </div>
 </template>
@@ -26,6 +37,8 @@ export default {
     return {
       loginUserName:'',
       loginPassword:'',
+      registUserName:'',
+      registPassword:'',
     }
   },
 }

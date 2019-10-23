bd3abf18c4098447f5e60813f912ebf9bea3e955
Healsc <873338654@qq.com>
2019-10-23 19:07:06 +0800

    shop

---
+++ shop/src/components/FooterBar.vue
@@ -0,0 +1,22 @@
+<template>
+  <van-tabbar v-model="active" @change="change">
+    <van-tabbar-item icon="home" to="/">首页</van-tabbar-item>
+    <van-tabbar-item icon="records" to="/category">分类</van-tabbar-item>
+    <van-tabbar-item icon="cart" to="/cart">购物车</van-tabbar-item>
+    <van-tabbar-item icon="contact" to="profile">我的</van-tabbar-item>
+  </van-tabbar>
+</template>
+
+<script>
+    export default {
+          data() {
+            return {
+                active: 0
+            };
+  },
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>

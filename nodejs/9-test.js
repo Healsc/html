8f93fb37e6bd22d7a64f475ce56521f7d551ebb6
Healsc <873338654@qq.com>
2019-10-28 22:13:27 +0800

    0

---
+++ nodejs/9-test.js
@@ -0,0 +1,12 @@
+var fs = require('fs');
+
+//var data = fs.readFileSync('./1.txt');
+fs.readFile('./1.txt','utf-8',function(err,data){
+    if(err){
+        console.log(err)
+    }else{
+        console.log(data)
+    }
+})
+console.log('end');
+console.log(__dirname);

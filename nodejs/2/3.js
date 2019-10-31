92f551f1b7b0655583ba9f1420c26edcac0bb31a
Healsc <873338654@qq.com>
2019-10-31 21:45:12 +0800

    nodejis

--- nodejs/2/3.js
+++ nodejs/2/3.js
@@ -1,6 +1,9 @@
 var http = require('http');
-
+var url = require('url');
 http.createServer(function(req,res){
+    var pathname = url.parse(req.url);
+    console.log(pathname);
+
     res.writeHead(200,{'Content-type':'text/html'});
     res.end("<h1>hello nodejs</h1>");
 }).listen(3000);

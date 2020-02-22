5e50cce03a7f17e68c4e72f6d4f8de5c0bbd3f74
Healsc <873338654@qq.com>
2020-02-22 11:07:45 +0800

    vue

--- 2020/vuepro/src/App.vue
+++ 2020/vuepro/src/App.vue
@@ -1,37 +1,18 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link> |
-      <router-link to="/mine">mine</router-link> |
-      <router-link to="/music">music</router-link> | 
-      <router-link to="/chat">chat</router-link> | 
-      <router-link to="/movie">movie</router-link> | 
-      <router-link to="/book">book</router-link> | 
-    </div>
-    <router-view/>
+  <div>
+    <Navigation/>
+    
   </div>
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
+<script>
+import Navigation from '@/components/Navigation.vue'
+export default {
+  components:{
+    Navigation
+  }
+}
+</script>
--- 2020/vuepro/src/router/index.js
+++ 2020/vuepro/src/router/index.js
@@ -1,25 +1,12 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+
 
 Vue.use(VueRouter)
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    component: () => import('../views/About.vue')
-  },{
-    path:"/mine",
-    name:"Mine",
-    component:()=>import('../views/Mine.vue')
-  },{
-    path:"/movie",
+    path:"/",
     name:"Movie",
     component:()=>import('../views/movie/Movie.vue')
   },{
